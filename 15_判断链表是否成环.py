49fde6b7ad9f606b085ac0c39c0f34981f8023ff
Hk4Fun <941222165chenhongwen@gmail.com>
2018-01-07 16:14:11 +0800

    增加测试用例

--- 15_判断链表是否成环.py
+++ 15_判断链表是否成环.py
@@ -5,7 +5,6 @@ __date__ = '2018/1/7 1:49'
 给一个链表，判断该链表是否成环
 '''
 '''主要思路：
-（链表要么单向，要么整体成环，不会局部有环，局部有环说明结点要有两个分支）
 定义两个指针，同时从链表的头结点出发，一个指针一次走一步，另一个一次走两步。
 如果走得快的指针追上了走得慢的指针说明链表成环；
 如果快指针来到了链表末尾都没有追上第一个指针，那么链表不成环
@@ -44,15 +43,18 @@ test_num = 0  # 总的测试数量
 time_pool = []  # 耗时
 
 
-def linkNodes(values, isCircle):
+def linkNodes(values, isCircle, linkIndex):
+    # linkIndex表示环的连接点索引，0表示尾结点与第一个结点链接成环，以此类推
     head = ListNode(None)
     lastNode = head
+    nodeList = []
     for val in values:
         newNode = ListNode(val)
+        nodeList.append(newNode)
         lastNode.next = newNode
         lastNode = newNode
     if isCircle:
-        lastNode.next = head.next
+        lastNode.next = nodeList[linkIndex]
     return head.next
 
 
@@ -79,11 +81,13 @@ def Test(testName, head, expected):
         print('Failed:测试不通过！\n')
 
 
-Test('Test1', linkNodes([1, 2, 3, 4, 5, 6, 7, 8], False), False)  # 单向链表，多个结点
-Test('Test2', linkNodes([1], False), False)  # 单向链表，单个结点
-Test('Test3', linkNodes([1, 2, 3, 4, 5, 6, 7, 8], True), True)  # 循环链表，多个结点
-Test('Test4', linkNodes([1], True), True)  # 循环链表，单个结点
-Test('Test5', linkNodes([], False), None)  # 空链表
+Test('Test1', linkNodes([1, 2, 3, 4, 5, 6, 7, 8], False, 0), False)  # 单向链表，多个结点
+Test('Test2', linkNodes([1], False, 0), False)  # 单向链表，单个结点
+Test('Test3', linkNodes([1, 2, 3, 4, 5, 6, 7, 8], True, 0), True)  # 循环链表，多个结点， 尾结点与头结点连接
+Test('Test4', linkNodes([1, 2, 3, 4, 5, 6, 7, 8], True, 3), True)  # 循环链表，多个结点， 尾结点与中间某个结点连接
+Test('Test5', linkNodes([1, 2, 3, 4, 5, 6, 7, 8], True, 7), True)  # 循环链表，多个结点， 尾结点与尾结点连接
+Test('Test6', linkNodes([1], True, 0), True)  # 循环链表，单个结点
+Test('Test7', linkNodes([], False, 0), None)  # 空链表
 
 print('测试结果：{}/{},{:.2f}%'.format(pass_num, test_num, (pass_num / test_num) * 100))
 if pass_num:
