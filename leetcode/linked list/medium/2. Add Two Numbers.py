fc3f261891e7aa75f855d07dbb8a5e37f12c930d
Hk4Fun <941222165chenhongwen@gmail.com>
2018-04-13 14:32:41 +0800

    linked list-medium

---
+++ leetcode/linked list/medium/2. Add Two Numbers.py
@@ -0,0 +1,88 @@
+__author__ = 'Hk4Fun'
+__date__ = '2018/4/13 13:56'
+'''题目描述：
+You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order and each of their nodes contain a single digit. 
+Add the two numbers and return it as a linked list.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+'''
+'''主要思路：
+时间O（n），空间O（n）
+'''
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = cur = ListNode(0)
+        p1, p2 = l1, l2
+        carry = 0
+        while p1 or p2 or carry:
+            if p1:
+                carry += p1.val
+                p1 = p1.next
+            if p2:
+                carry += p2.val
+                p2 = p2.next
+            # 注意python的连续赋值是从左到右，这里相当于
+            # tmp = ListNode(carry % 10)
+            # cur.next = tmp
+            # cur = tmp
+            cur.next = cur = ListNode(carry % 10)
+            carry //= 10
+        return dummy.next
+
+
+# ================================测试代码================================
+from Test import Test
+
+
+class MyTest(Test):
+    def my_test_code(self):
+        self.debug = False  # debug为True时每个测试用例只测试一遍，默认情况下关闭debug模式
+        self.TEST_NUM = 1  # 单个测试用例的测试次数, 只有在debug为False的情况下生效
+        testArgs = []
+        # 只需在此处填写自己的测试代码
+        # testArgs中每一项是一次测试，每一项由两部分构成
+        # 第一部分为被测试函数的参数，第二部分只有最后一个，为正确答案
+
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+
+        return testArgs
+
+    def convert(self, result, *func_arg):
+        # 在此处填写转换函数，将测试结果转换成其他形式
+        return result
+
+    def checked(self, result, expected, *func_arg):
+        # 在此处填写比较器，测试返回的结果是否正确
+        return result == expected
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    MyTest(solution=solution).start_test()
