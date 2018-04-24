bb8f004890718044cda5043ab8bbc302fe4ac0e7
Hk4Fun <941222165chenhongwen@gmail.com>
2018-04-24 23:44:41 +0800

    tree-easy

---
+++ leetcode/tree/easy/617. Merge Two Binary Trees.py
@@ -0,0 +1,74 @@
+__author__ = 'Hk4Fun'
+__date__ = '2018/4/24 23:43'
+'''题目描述：
+Given two binary trees and imagine that when you put one of them to cover the other, 
+some nodes of the two trees are overlapped while the others are not.
+You need to merge them into a new binary tree. 
+The merge rule is that if two nodes overlap, 
+then sum node values up as the new value of the merged node. 
+Otherwise, the NOT null node will be used as the node of new tree.
+
+Example 1:
+Input: 
+	Tree 1                     Tree 2                  
+          1                         2                             
+         / \                       / \                            
+        3   2                     1   3                        
+       /                           \   \                      
+      5                             4   7                  
+Output: 
+Merged tree:
+	     3
+	    / \
+	   4   5
+	  / \   \ 
+	 5   4   7
+Note: The merging process must start from the root nodes of both trees.
+'''
+'''主要思路：
+时间O（），空间O（）
+'''
+
+
+class Solution:
+    pass
+
+
+# ================================测试代码================================
+from Test import Test
+
+
+class MyTest(Test):
+    def my_test_code(self):
+        self.debug = False  # debug为True时每个测试用例只测试一遍，默认情况下关闭debug模式
+        self.TEST_NUM = 1  # 单个测试用例的测试次数, 只有在debug为False的情况下生效
+        testArgs = []
+        # 只需在此处填写自己的测试代码
+        # testArgs中每一项是一次测试，每一项由两部分构成
+        # 第一部分为被测试函数的参数，第二部分只有最后一个，为正确答案
+
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+
+        return testArgs
+
+    def convert(self, result, *func_arg):
+        # 在此处填写转换函数，将测试结果转换成其他形式
+        return result
+
+    def checked(self, result, expected, *func_arg):
+        # 在此处填写比较器，测试返回的结果是否正确
+        return result == expected
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    MyTest(solution=solution).start_test()
