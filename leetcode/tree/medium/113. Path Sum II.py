7b38cc3a1018f9a96e174e39fad778c5f54c0b24
Hk4Fun <941222165chenhongwen@gmail.com>
2018-04-28 00:37:22 +0800

    tree-medium

---
+++ leetcode/tree/medium/113. Path Sum II.py
@@ -0,0 +1,99 @@
+__author__ = 'Hk4Fun'
+__date__ = '2018/4/27 21:05'
+'''题目描述：
+Given a binary tree and a sum, 
+find all root-to-leaf paths where each path's sum equals the given sum.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given the below binary tree and sum = 22,
+
+      5
+     / \
+    4   8
+   /   / \
+  11  13  4
+ /  \    / \
+7    2  5   1
+Return:
+
+[
+   [5,4,11,2],
+   [5,8,4,5]
+]
+
+'''
+'''主要思路：
+时间O（n），空间O（n）
+leetcode/tree/easy/112. Path Sum.py
+'''
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    """
+    :type root: TreeNode
+    :type sum: int
+    :rtype: List[List[int]]
+    """
+
+    def pathSum(self, root, sum):
+        def path(root, sum, res):
+            if not root: return
+            if not root.left and not root.right and sum == root.val:
+                res.append(root.val)
+                self.res.append(res)
+            path(root.left, sum - root.val, res + [root.val])
+            path(root.right, sum - root.val, res + [root.val])
+
+        self.res = []
+        path(root, sum, [])
+        return self.res
+
+
+# ================================测试代码================================
+from Test import Test
+
+
+class MyTest(Test):
+    def my_test_code(self):
+        self.debug = False  # debug为True时每个测试用例只测试一遍，默认情况下关闭debug模式
+        self.TEST_NUM = 1  # 单个测试用例的测试次数, 只有在debug为False的情况下生效
+        testArgs = []
+        # 只需在此处填写自己的测试代码
+        # testArgs中每一项是一次测试，每一项由两部分构成
+        # 第一部分为被测试函数的参数，第二部分只有最后一个，为正确答案
+
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+
+        return testArgs
+
+    def convert(self, result, *func_arg):
+        # 在此处填写转换函数，将测试结果转换成其他形式
+        return result
+
+    def checked(self, result, expected, *func_arg):
+        # 在此处填写比较器，测试返回的结果是否正确
+        return result == expected
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    MyTest(solution=solution).start_test()
---
+++ leetcode/tree/medium/114. Flatten Binary Tree to Linked List.py
@@ -0,0 +1,126 @@
+__author__ = 'Hk4Fun'
+__date__ = '2018/4/27 21:44'
+'''题目描述：
+Given a binary tree, flatten it to a linked list in-place.
+
+For example, given the following tree:
+
+    1
+   / \
+  2   5
+ / \   \
+3   4   6
+The flattened tree should look like:
+
+1
+ \
+  2
+   \
+    3
+     \
+      4
+       \
+        5
+         \
+          6
+'''
+'''主要思路：
+时间O（n），空间O（n）
+思路1：前序遍历，设置一个pre保存前一个结点，
+      由于进入左子树会改变当前结点右孩子的连接，
+      所以在进入左子树前记得先保存当前结点的右孩子
+      
+思路2：思路1之所以要先保存当前结点的右孩子是因为前序遍历的特点，cur->left->right
+      那么可以反过来，用一种类似后序遍历的遍历方式：right->left->cur
+      这样就可以不用先保存当前结点的右孩子了
+      
+思路3：仿照morris遍历的思路
+      
+'''
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    """
+    :type root: TreeNode
+    :rtype: void Do not return anything, modify root in-place instead.
+    """
+
+    def flatten1(self, root):
+        def flat(root):
+            if not root: return
+            self.pre.left, self.pre.right = None, root
+            self.pre = root
+            right = root.right  # 进入左子树前先保存右孩子
+            flat(root.left)
+            flat(right)
+
+        self.pre = TreeNode(0)
+        flat(root)
+
+    def flatten2(self, root):
+        def dfs(root):
+            if not root: return
+            dfs(root.right)
+            dfs(root.left)
+            root.right, root.left = self.prev, None
+            self.prev = root
+
+        self.prev = None  # 思路1的pre初始化不能为None
+        dfs(root)
+
+    def flatten3(self, root):
+        cur = root
+        while cur:
+            if cur.left:
+                pre = cur.left
+                while pre.right: pre = pre.right # 来到左子树的最右结点
+                pre.right = cur.right # 把当前结点的右子树连接到最右结点的右指针
+                cur.left, cur.right = None, cur.left # 把当前结点的左子树连接到右指针
+            cur = cur.right # 继续访问右子树
+
+
+# ================================测试代码================================
+from Test import Test
+
+
+class MyTest(Test):
+    def my_test_code(self):
+        self.debug = False  # debug为True时每个测试用例只测试一遍，默认情况下关闭debug模式
+        self.TEST_NUM = 1  # 单个测试用例的测试次数, 只有在debug为False的情况下生效
+        testArgs = []
+        # 只需在此处填写自己的测试代码
+        # testArgs中每一项是一次测试，每一项由两部分构成
+        # 第一部分为被测试函数的参数，第二部分只有最后一个，为正确答案
+
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+
+        return testArgs
+
+    def convert(self, result, *func_arg):
+        # 在此处填写转换函数，将测试结果转换成其他形式
+        return result
+
+    def checked(self, result, expected, *func_arg):
+        # 在此处填写比较器，测试返回的结果是否正确
+        return result == expected
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    MyTest(solution=solution).start_test()
