564ba0aaaa9ccd70de6ba919be96a03f1bfd4b54
Hk4Fun <941222165chenhongwen@gmail.com>
2018-05-12 02:17:46 +0800

    hash table - mediu

---
+++ leetcode/hash table/medium/36. Valid Sudoku.py
@@ -0,0 +1,121 @@
+__author__ = 'Hk4Fun'
+__date__ = '2018/5/12 0:39'
+'''题目描述：
+Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+A partially filled sudoku which is valid.
+
+The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+
+Example 1:
+
+Input:
+[
+  ["5","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+]
+Output: true
+Example 2:
+
+Input:
+[
+  ["8","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being 
+    modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+The given board contain only digits 1-9 and the character '.'.
+The given board size is always 9x9.
+'''
+'''主要思路：
+三张表分别存放行、列、块，检查每个数是否已经出现在对应的行列块中
+'''
+
+from collections import Counter
+
+
+class Solution:
+    def isValidSudoku1(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+        row = [{}, {}, {}, {}, {}, {}, {}, {}, {}]  # 字典的in检索O(1)
+        col = [{}, {}, {}, {}, {}, {}, {}, {}, {}]
+        block = [{}, {}, {}, {}, {}, {}, {}, {}, {}]
+
+        for i in range(len(board)):
+            for j in range(len(board)):
+                num = board[i][j]
+                k = 3 * (i // 3) + (j // 3)  # k为块的索引
+                if num == '.':
+                    continue
+                elif num not in row[i] and num not in col[j] and num not in block[k]:
+                    row[i][num] = 1  # 1只起到标记作用
+                    col[j][num] = 1
+                    block[k][num] = 1
+                else:
+                    return False
+        return True
+
+
+# ================================测试代码================================
+from Test import Test
+
+
+class MyTest(Test):
+    def my_test_code(self):
+        self.debug = False  # debug为True时每个测试用例只测试一遍，默认情况下关闭debug模式
+        self.TEST_NUM = 1  # 单个测试用例的测试次数, 只有在debug为False的情况下生效
+        testArgs = []
+        # 只需在此处填写自己的测试代码
+        # testArgs中每一项是一次测试，每一项由两部分构成
+        # 第一部分为被测试函数的参数，第二部分只有最后一个，为正确答案
+
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+
+        return testArgs
+
+    def convert(self, result, *func_arg):
+        # 在此处填写转换函数，将测试结果转换成其他形式
+        return result
+
+    def checked(self, result, expected, *func_arg):
+        # 在此处填写比较器，测试返回的结果是否正确
+        return result == expected
+
+
+if __name__ == '__main__':
+    MyTest(Solution()).start_test()
---
+++ leetcode/hash table/medium/49. Group Anagrams.py
@@ -0,0 +1,79 @@
+__author__ = 'Hk4Fun'
+__date__ = '2018/5/12 2:12'
+'''题目描述：
+Given an array of strings, group anagrams together.
+
+Example:
+
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note:
+
+All inputs will be in lowercase.
+The order of your output does not matter.
+'''
+'''主要思路：
+时间O（NK*logK），空间O（N*k）, N表示strs的str个数，k表示最长str的长度
+利用sorted为每个单词排序，然后映射到dict中进行gruop，这样互为anagrams的排完序后一样，
+一定会映射到同一组。由于python的dict键必须可哈希，所以需要在sorted往后join回去或者用tuple来保证键的可哈希
+'''
+
+
+class Solution:
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        res = {}
+        for s in strs:
+            key = ''.join(sorted(s)) # key must hashable
+            if key in res:
+                res[key].append(s)
+            else:
+                res[key] = [s]
+        return list(res.values())
+
+
+# ================================测试代码================================
+from Test import Test
+
+
+class MyTest(Test):
+    def my_test_code(self):
+        self.debug = False  # debug为True时每个测试用例只测试一遍，默认情况下关闭debug模式
+        self.TEST_NUM = 1  # 单个测试用例的测试次数, 只有在debug为False的情况下生效
+        testArgs = []
+        # 只需在此处填写自己的测试代码
+        # testArgs中每一项是一次测试，每一项由两部分构成
+        # 第一部分为被测试函数的参数，第二部分只有最后一个，为正确答案
+
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+
+        return testArgs
+
+    def convert(self, result, *func_arg):
+        # 在此处填写转换函数，将测试结果转换成其他形式
+        return result
+
+    def checked(self, result, expected, *func_arg):
+        # 在此处填写比较器，测试返回的结果是否正确
+        return result == expected
+
+
+if __name__ == '__main__':
+    MyTest(Solution()).start_test()
