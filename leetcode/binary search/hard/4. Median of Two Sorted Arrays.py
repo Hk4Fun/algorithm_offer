b5cdc60e7ef4b4edbc0104c079dbd4e7e6d2ba3b
Hk4Fun <941222165chenhongwen@gmail.com>
2018-07-15 00:07:28 +0800

    binary search-hard

---
+++ leetcode/binary search/hard/4. Median of Two Sorted Arrays.py
@@ -0,0 +1,105 @@
+__author__ = 'Hk4Fun'
+__date__ = '2018/7/14 16:17'
+'''题目描述：
+There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+Example 1:
+nums1 = [1, 3]
+nums2 = [2]
+
+The median is 2.0
+Example 2:
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+The median is (2 + 3)/2 = 2.5
+'''
+'''主要思路：
+时间O（log(min(m,n)），空间O（1）
+https://leetcode.com/problems/median-of-two-sorted-arrays/discuss/2481/Share-my-O(log(min(mn))-solution-with-explanation
+其本质就是确定A中分割点i的位置，此时B中的分割点j也就确定，满足B[j-1] <= A[i] and A[i-1] <= B[j]
+然后考虑如何舍弃另外一半以及四种边界情况
+'''
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        A, B = nums1, nums2
+        m, n = len(A), len(B)
+        if m > n: A, B, m, n = B, A, n, m
+        imin, imax, half_len = 0, m, (m + n + 1) // 2
+        while imin <= imax:
+            i = (imin + imax) // 2
+            j = half_len - i
+            if i < m and B[j - 1] > A[i]:
+                # i is too small, must increase it
+                imin = i + 1
+            elif i > 0 and A[i - 1] > B[j]:
+                # i is too big, must decrease it
+                imax = i - 1
+            else:
+                # i is perfect
+                if i == 0:
+                    max_of_left = B[j - 1]
+                elif j == 0:
+                    max_of_left = A[i - 1]
+                else:
+                    max_of_left = max(A[i - 1], B[j - 1])
+
+                if (m + n) & 1 == 1:
+                    return max_of_left
+
+                if i == m:
+                    min_of_right = B[j]
+                elif j == n:
+                    min_of_right = A[i]
+                else:
+                    min_of_right = min(A[i], B[j])
+
+                return (max_of_left + min_of_right) / 2
+
+
+# ================================测试代码================================
+from Test import Test
+
+
+class MyTest(Test):
+    def my_test_code(self):
+        self.debug = False  # debug为True时每个测试用例只测试一遍，默认情况下关闭debug模式
+        self.TEST_NUM = 1  # 单个测试用例的测试次数, 只有在debug为False的情况下生效
+        testArgs = []
+        # 只需在此处填写自己的测试代码
+        # testArgs中每一项是一次测试，每一项由两部分构成
+        # 第一部分为被测试函数的参数，第二部分只有最后一个，为正确答案
+
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+
+        return testArgs
+
+    def convert(self, result, *func_arg):
+        # 在此处填写转换函数，将测试结果转换成其他形式
+        return result
+
+    def checked(self, result, expected, *func_arg):
+        # 在此处填写比较器，测试返回的结果是否正确
+        return result == expected
+
+
+if __name__ == '__main__':
+    MyTest(Solution()).start_test()
