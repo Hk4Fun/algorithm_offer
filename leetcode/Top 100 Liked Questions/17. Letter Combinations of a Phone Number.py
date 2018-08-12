797f9334ece0e5d6ffb0cecbc714f7d01850f1da
Hk4Fun <941222165chenhongwen@gmail.com>
2018-08-12 20:33:17 +0800

    top 100 liked

---
+++ leetcode/Top 100 Liked Questions/17. Letter Combinations of a Phone Number.py
@@ -0,0 +1,89 @@
+__author__ = 'Hk4Fun'
+__date__ = '2018/8/12 16:57'
+'''题目描述：
+Given a string containing digits from 2-9 inclusive, 
+return all possible letter combinations that the number could represent.
+A mapsping of digit to letters (just like on the telephone buttons) is given below. 
+{'2':'abc', '3':'def', '4':'ghi', '5':'jkl', '6':'mno', '7':'pqrs', '8':'tuv', '9':'wxyz'}
+Note that 1 does not maps to any letters.
+'''
+'''主要思路：
+回溯法
+'''
+
+from functools import reduce
+
+
+class Solution:
+    """
+    :type digits: str
+    :rtype: List[str]
+    """
+
+    def letterCombinations1(self, digits):
+        if not digits: return []
+        maps = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
+
+        def bt(idx):
+            if idx == len(digits) - 1:
+                return list(maps[digits[idx]])
+            res = []
+            for ch in maps[digits[idx]]:
+                for s in bt(idx + 1):
+                    res.append(ch + s)
+            return res
+
+        return bt(0)
+
+    def letterCombinations2(self, digits):  # 简化版，使用reduce
+        if not digits: return []
+        maps = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+        return reduce(lambda acc, digit: [x + y for x in acc for y in maps[digit]], digits, [''])
+
+
+# ================================测试代码================================
+from Test import Test
+
+
+class MyTest(Test):
+    def my_test_code(self):
+        self.debug = True  # debug为True时每个测试用例只测试一遍，默认情况下关闭debug模式
+        self.TEST_NUM = 1  # 单个测试用例的测试次数, 只有在debug为False的情况下生效
+        testArgs = []
+        # 只需在此处填写自己的测试代码
+        # testArgs中每一项是一次测试，每一项由两部分构成
+        # 第一部分为被测试函数的参数，第二部分只有最后一个，为正确答案
+
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+        testArgs.append([])
+
+        return testArgs
+
+    def convert(self, result, *func_arg):
+        # 在此处填写转换函数，将测试结果转换成其他形式
+        return result
+
+    def checked(self, result, expected, *func_arg):
+        # 在此处填写比较器，测试返回的结果是否正确
+        return result == expected
+
+
+if __name__ == '__main__':
+    MyTest(Solution()).start_test()
---
+++ leetcode/Top 100 Liked Questions/200. Number of Islands.py
@@ -0,0 +1,116 @@
+__author__ = 'Hk4Fun'
+__date__ = '2018/8/12 17:04'
+'''题目描述：
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. 
+An island is surrounded by water and is formed by connecting adjacent lands horizontally 
+or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+
+Input:
+11110
+11010
+11000
+00000
+
+Output: 1
+Example 2:
+
+Input:
+11000
+11000
+00100
+00011
+
+Output: 3
+'''
+'''主要思路：
+思路1：union find (quick-union)
+
+思路2：dfs
+
+'''
+
+
+class Solution:
+    """
+    :type grid: List[List[str]]
+    :rtype: int
+    """
+
+    def numIslands1(self, grid):
+        if not grid: return 0
+        rows, cols = len(grid), len(grid[0])
+        self.count = rows * cols
+        self.id = list(range(rows * cols))
+
+        def find(p):
+            while self.id[p] != p:
+                p = self.id[p]
+            return p
+
+        def union(p, q):  # quick-union
+            pid, qid = find(p), find(q)
+            if pid == qid: return
+            self.id[pid] = qid
+            self.count -= 1
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == '1':
+                    p = i * cols + j
+                    if i - 1 >= 0 and grid[i - 1][j] == '1':
+                        union(p, (i - 1) * cols + j)
+                    if i + 1 < rows and grid[i + 1][j] == '1':
+                        union(p, (i + 1) * cols + j)
+                    if j - 1 >= 0 and grid[i][j - 1] == '1':
+                        union(p, i * cols + (j - 1))
+                    if j + 1 < cols and grid[i][j + 1] == '1':
+                        union(p, i * cols + (j + 1))
+                else:  # 0不是岛屿，前面 self.count=rows*cols 全算进去了，这里减回来
+                    self.count -= 1
+        return self.count
+
+    def numIslands2(self, grid):
+        def dfs(i, j):
+            if 0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j] == '1':
+                grid[i][j] = '0'  # 消掉该岛屿
+                # 消掉周围相连的岛屿，注意这里必须用list来启动直接返回生成器的map，py2下可以不用list
+                list(map(dfs, (i - 1, i + 1, i, i), (j, j, j - 1, j + 1)))
+                return 1
+            return 0
+
+        return sum(dfs(i, j) for i in range(len(grid)) for j in range(len(grid[0])))
+
+
+# ================================测试代码================================
+from Test import Test
+
+
+class MyTest(Test):
+    def my_test_code(self):
+        self.debug = True  # debug为True时每个测试用例只测试一遍，默认情况下关闭debug模式
+        self.TEST_NUM = 1  # 单个测试用例的测试次数, 只有在debug为False的情况下生效
+        testArgs = []
+        # 只需在此处填写自己的测试代码
+        # testArgs中每一项是一次测试，每一项由两部分构成
+        # 第一部分为被测试函数的参数，第二部分只有最后一个，为正确答案
+
+        testArgs.append([[["1", "1", "1", "1", "0"],
+                          ["1", "1", "0", "1", "0"],
+                          ["1", "1", "0", "0", "0"],
+                          ["0", "0", "0", "0", "0"]], 1])
+
+        return testArgs
+
+    def convert(self, result, *func_arg):
+        # 在此处填写转换函数，将测试结果转换成其他形式
+        return result
+
+    def checked(self, result, expected, *func_arg):
+        # 在此处填写比较器，测试返回的结果是否正确
+        return result == expected
+
+
+if __name__ == '__main__':
+    MyTest(Solution()).start_test()
