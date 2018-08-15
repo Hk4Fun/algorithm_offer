ff913f37d7801363f378094494be10d4afc16ebf
Hk4Fun <941222165chenhongwen@gmail.com>
2018-08-15 21:57:07 +0800

    add solution

--- leetcode/two pointers/medium/3. Longest Substring Without Repeating Characters.py
+++ leetcode/two pointers/medium/3. Longest Substring Without Repeating Characters.py
@@ -11,12 +11,14 @@ Note that the answer must be a substring, "pwke" is a subsequence and not a subs
 '''
 '''主要思路：
 时间O（n），空间O（1）（字母种类有限，所以空间为常数）
-keep a hashmap which stores the characters in string as keys and their positions as values, 
-and keep two pointers which define the max substring. 
-move the right pointer to scan through the string , 
-and meanwhile update the hashmap. If the character is already in the hashmap, 
-then move the left pointer to the right of the same character last found. 
-Note that the two pointers can only move forward.
+用两个指针始终记录不重复字符串的头start和尾tail，tail一直往右边扫描
+每当遇到重复的字符（需要一个哈希表记录每个字符曾经出现过的最右的索引），
+设该重复字符之前出现的位置为i，如果 start <= i < tail，
+则只需把头指针移到 i + 1 的位置即可，否则（i < start）接着往右扫描
+当然循环每一步都要更新当前字符出现的最新位置，并且更新最大长度
+这里可以稍微优化一下：当出现重复字符并且 start <= i < tail 时，
+我们令start = i + 1，则字符串长度一定减小了，可以不用更新字符串长度
+换句话说，只有start不往右移时我们才去更新最大长度
 '''
 
 
@@ -28,12 +30,12 @@ class Solution:
         """
         maxLen = start = 0
         used = {}  # 记录每个字母最近出现的位置
-        for i, v in enumerate(s):  # i一直往右遍历
+        for end, v in enumerate(s):  # i一直往右遍历
             if v in used and start <= used[v]:  # 之前出现过该字母并且最近出现的位置在start到i之间
                 start = used[v] + 1  # 将start移到上次出现位置的右边
-            else:
-                maxLen = max(maxLen, i - start + 1)
-            used[v] = i  # 新增或更新每个字母最近出现的位置
+            else: # 如果start被右移了就没必要更新maxLen了，因为肯定比原来的小
+                maxLen = max(maxLen, end - start + 1)
+            used[v] = end  # 新增或更新每个字母最近出现的位置
         return maxLen
 
 
