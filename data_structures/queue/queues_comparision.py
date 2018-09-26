8a6c58e3a294a2ec65f3636fa4684e059a2ab1a2
Hk4Fun <941222165chenhongwen@gmail.com>
2018-09-27 03:03:49 +0800

    test deque

--- data_structures/queue/queues_comparision.py
+++ data_structures/queue/queues_comparision.py
@@ -5,11 +5,13 @@ import time
 
 from array_queue import ArrayQueue
 from loop_queue import LoopQueue
+from array_deque import ArrayDeque
+from loop_deque import LoopDeque
 
 TEST_NUM = 10000
 
 
-def exec_time(queue):
+def queue_exec_time(queue):
     start = time.time()
 
     for i in range(TEST_NUM):
@@ -23,8 +25,32 @@ def exec_time(queue):
     return end - start
 
 
+def deque_exec_time(deque):
+    start = time.time()
+
+    for i in range(TEST_NUM // 2):
+        deque.push_tail(i)
+
+    for i in range(TEST_NUM // 2):
+        deque.push_front(i)
+
+    for i in range(TEST_NUM // 2):
+        deque.pop_tail()
+
+    for i in range(TEST_NUM // 2):
+        deque.pop_front()
+
+    end = time.time()
+
+    return end - start
+
+
 if __name__ == '__main__':
     queue = ArrayQueue(TEST_NUM)
-    print('ArrayQueue, time: {}'.format(exec_time(queue)))
+    print('ArrayQueue, time: {}'.format(queue_exec_time(queue)))
     queue = LoopQueue(TEST_NUM)
-    print('LoopQueue, time: {}'.format(exec_time(queue)))
+    print('LoopQueue, time: {}'.format(queue_exec_time(queue)))
+    deque = ArrayDeque(TEST_NUM)
+    print('ArrayDeque, time: {}'.format(deque_exec_time(deque)))
+    deque = LoopDeque(TEST_NUM)
+    print('LoopDeque, time: {}'.format(deque_exec_time(deque)))
